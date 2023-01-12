18529d3da3adc51374e4a21cc864c2dc3eb9662f
Akshay verma <akshayverma88746@gmail.com>
2023-01-13 00:08:07 +0530

    added forms

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash # flash for flashing messages
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired 
 # Create a flask object
 app = Flask(__name__)
-
+app.config['SECRET_KEY']="nothing"
+# Create a form class
+class NameForm(FlaskForm):
+    name = StringField("Please enter your name", validators=[DataRequired()])   # Create a text field for input
+    submit = SubmitField("Submit")
 # Create a route 
 @app.route('/')
 def index():
@@ -27,3 +33,18 @@ def not_found(e):
 @app.errorhandler(500)
 def server(e):
     return render_template("500.html"), 500
+    
+#Create name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None;
+    form = NameForm()
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+        flash("Form Submission Successful")
+    return render_template('name.html', name = name, form = form)
+
+    
+
