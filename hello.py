e8d87c388d2e58cc5042191d63929d8d84e9f120
Akshay verma <akshayverma88746@gmail.com>
2023-01-14 23:49:51 +0530

    added database for login functionality

--- hello.py
+++ hello.py
@@ -1,15 +1,59 @@
 from flask import Flask, render_template, flash # flash for flashing messages
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired 
+from wtforms.validators import DataRequired, email_validator
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 # Create a flask object
 app = Flask(__name__)
+app.app_context().push()
+# Add data base to our app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# Secret key
 app.config['SECRET_KEY']="nothing"
+
+# Initialize the database
+db = SQLAlchemy(app)
+# Create Model of the database
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable= False)
+    email = db.Column(db.String(200), nullable = False, unique = True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Create String
+    def __repr__(self):
+        return '<Name %r>' %self.name
+
+# Create a form class
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])# Create a text field for input
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    
 # Create a form class
 class NameForm(FlaskForm):
     name = StringField("Please enter your name", validators=[DataRequired()])   # Create a text field for input
     submit = SubmitField("Submit")
 # Create a route 
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email = form.email.data).first()
+        if user is None:
+            user = Users(name= form.name.data, email= form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User Added Successfully")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form = form, name = name, our_users= our_users)
+
 @app.route('/')
 def index():
     first_name = "Akshay"
---
+++ static/js/myjs.js
@@ -0,0 +1,1 @@
+document.getElementById('demo').innerHTML = 'This is created using Basic JAVA script'
