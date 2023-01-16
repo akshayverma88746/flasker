6558068f55a8a2de180cfcb6cb919033880c46ee
Akshay verma <akshayverma88746@gmail.com>
2023-01-16 23:50:52 +0530

    delete option added

--- hello.py
+++ hello.py
@@ -29,7 +29,21 @@ class Users(db.Model):
     # Create String
     def __repr__(self):
         return '<Name %r>' %self.name
-
+# Delete record
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None;
+    form = UserForm()
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User Deleted Successfully !!")
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("add_user.html", form = form, name = name, our_users= our_users)
+    except:
+        flash("There was a problem deleting the user.... Try again")
+        return render_template("add_user.html", form = form, name = name, our_users= our_users)
 # Create a form class
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])# Create a text field for input
